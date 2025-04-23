b3aa94b6c86d3875ddfdc42581c43640e5e69bdc
김의중 <ejk5148@gmail.com>
2025-04-23 14:42:24 +0900

    [init] 스프링부트 프로젝트 생성

---
+++ server/src/main/java/com/fairytale/FairyTale/FairyTaleApplication.java
@@ -0,0 +1,13 @@
+package com.fairytale.FairyTale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairyTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairyTaleApplication.class, args);
+	}
+
+}
