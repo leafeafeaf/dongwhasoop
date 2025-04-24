40c438f6c54d2844600bb472c9feb7f93ca380aa
김의중 <ejk5148@gmail.com>
2025-04-24 17:49:25 +0900

    [fix] BookController 엔드포인트 설정

--- server/src/main/java/com/fairytale/FairyTale/domain/book/presentation/BookController.java
+++ server/src/main/java/com/fairytale/FairyTale/domain/book/presentation/BookController.java
@@ -1,4 +1,11 @@
 package com.fairytale.FairyTale.domain.book.presentation;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/books")
 public class BookController {
 }
