e916413d382eb9a0e71a8de12073158591c0634a
오승열 <tmdduf785@naver.com>
2025-04-28 10:56:56 +0900

    feat: book,song Intro Page [#S12P31B202-81]

--- frontend/src/pages/Intro.tsx
+++ frontend/src/pages/Intro.tsx
@@ -23,30 +23,30 @@ function Intro() {
       {/* 뒤로가기 */}
       <BackButton to="/booklist" />
 
-      {/* 상단 텍스트 */}
-      <h1 className="text-[5vh] sm:text-[6vh] lg:text-[7vh] font-bazzi text-black text-outline text-center mt-[6vh]">
+      <h1 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 text-[4vh] sm:text-[6vh] lg:text-[10vh] font-bazzi text-black text-outline-sm text-center mt-[15vh]">
         어떻게 읽을까요?
       </h1>
 
-      {/* 콘텐츠 영역 */}
-      <div className="flex flex-col lg:flex-row justify-center items-center h-full gap-[5vh] mt-[5vh] lg:mt-[4vh]">
+      <div className="absolute left-1/2 -translate-x-1/2 flex flex-col lg:flex-row justify-center items-center h-full gap-[10vh] lg:mt-[10vh]">
         {/* 책 표지 + 제목 */}
         <div className="flex flex-col items-center">
           <img
             src={selectedBook.cover}
             alt="책 표지"
-            className="w-[45vw] sm:w-[35vw] lg:w-[20vw] max-w-[400px] rounded-xl border-4 border-white shadow-lg"
+            className="w-[40vw] sm:w-[30vw] lg:w-[20vw] max-w-[700px] rounded-xl border-4 border-white shadow-lg"
           />
-          <h2 className="mt-4 text-[4vh] sm:text-[5vh] font-bazzi text-center text-[#4e4e4e] text-outline">
+
+          <h2 className="mt-4 text-[4vh] sm:text-[5vh] font-bazzi text-center text-[#4e4e4e] text-outline-sm">
             {selectedBook.title}
           </h2>
         </div>
 
-        {/* 버튼 영역 */}
-        <div className="flex flex-col gap-[4vh] mt-[4vh] lg:mt-0">
+        {/* 동요, 동화 버튼 */}
+        <div className="flex flex-col gap-[4vh] mt-[4vh] lg:mb-[15vh]">
           <button onClick={() => navigate(`/introsong/${id}`)}>
             <img src={Song} alt="동요" className="w-[45vw] sm:w-[35vw] lg:w-[18vw] max-w-[400px]" />
           </button>
+
           <button onClick={() => navigate(`/introbook/${id}`)}>
             <img src={Fairytale} alt="동화" className="w-[45vw] sm:w-[35vw] lg:w-[18vw] max-w-[400px]" />
           </button>
