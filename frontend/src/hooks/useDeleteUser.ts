ec8ab9ac0ac68046e922cc316f3772e4613896e3
오승열 <tmdduf785@naver.com>
2025-05-02 12:46:48 +0900

    feat: axiosinstance RefreshToken [#S12P31B202-101]

--- frontend/src/hooks/useDeleteUser.ts
+++ frontend/src/hooks/useDeleteUser.ts
@@ -1,13 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { deleteUser } from "../api/authApi";
+import { useAppStore } from "../stores/useAppStore";
 
 // 회원 탈퇴
 export const useDeleteUser = () => {
+  const resetStore = useAppStore.getState();
+
   return useMutation({
     mutationFn: (code: string) => deleteUser(code),
 
     onSuccess: () => {
       localStorage.clear();
+      resetStore.setSelectedChildId(null);
+      resetStore.setUserProfile(null);
     },
   });
 };
--- frontend/src/hooks/useLogin.ts
+++ frontend/src/hooks/useLogin.ts
@@ -1,15 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../api/authApi";
 import { LoginApiResponse } from "../types/auth";
+import { useAppStore } from "../stores/useAppStore";
 
 // 로그인
 export const useLogin = () => {
+  const setUserProfile = useAppStore((state) => state.setUserProfile);
+
   return useMutation({
     mutationFn: (idToken: string) => login(idToken),
 
-    onSuccess: (data) => {
+    onSuccess: (data: LoginApiResponse["data"]) => {
       localStorage.setItem("accessToken", data.accessToken);
       localStorage.setItem("refreshToken", data.refreshToken);
+
+      setUserProfile({
+        isNew: data.isNew,
+      });
     },
 
     onError: (error) => {
