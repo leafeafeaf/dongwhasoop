eaca2ccdaf7512f25e87ac9f53a78a4f078f928f
chilly003 <chfhchf03@naver.com>
2025-05-07 10:13:53 +0900

    fix : bookloading 페이지 수정 완료

--- frontend/src/pages/BookLoading.tsx
+++ frontend/src/pages/BookLoading.tsx
@@ -1,20 +1,6 @@
-import { useEffect, useRef } from "react";
 import bookintrobackground from "../assets/images/bookintro/bookintrobackground.webp";
-import bookDummy from "../data/bookDummy";
-import HTMLFlipBook from "react-pageflip";
 
 const BookLoading = () => {
-  const bookRef = useRef<any>(null);
-
-  useEffect(() => {
-    const flipInterval = setInterval(() => {
-      if (bookRef.current) {
-        bookRef.current.pageFlip().flipNext();
-      }
-    }, 1900); // 1초마다 페이지 넘김
-
-    return () => clearInterval(flipInterval);
-  }, []);
 
   return (
     <div
@@ -27,42 +13,6 @@ const BookLoading = () => {
       </div>
 
       <div className="flex justify-center items-center mt-[4vw] relative animate-bounce-infinite3 ">
-        {/* Book background */}
-        <div className="absolute bg-[#2c3e50] rounded-lg shadow-2xl"
-          style={{
-            width: 'calc(1400px + 40px)',
-            height: 'calc(900px + 40px)',
-            boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.3)',
-          }}
-        />
-        <HTMLFlipBook
-          ref={bookRef}
-          width={window.innerWidth < 1280 ? 500 : 700}
-          height={window.innerWidth < 1280 ? 700 : 900}
-          size="stretch"
-          minWidth={400}
-          maxWidth={700}
-          minHeight={600}
-          maxHeight={900}
-          className="z-10"
-          style={{}} // 필수
-          startPage={0} // 필수
-          drawShadow={true} // 필수
-          flippingTime={1000} // 필수
-        >
-          {bookDummy.slice(1, 36).map((book) => (
-            <div
-              key={book.id}
-              className="relative bg-white rounded-lg shadow-lg"
-            >
-              <img
-                src={book.cover}
-                alt={book.title}
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
-          ))}
-        </HTMLFlipBook>
 
       </div>
 
