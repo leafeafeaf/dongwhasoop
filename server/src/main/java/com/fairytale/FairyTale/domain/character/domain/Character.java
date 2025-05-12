0ca4061b70d68b0f35259913dac67bf9b0c0e121
김성민 <sungmin0283@gmail.com>
2025-05-12 15:35:25 +0900

    [fix] 캐릭터 엔티티 수정 #S12P31B202-117

--- server/src/main/java/com/fairytale/FairyTale/domain/character/domain/Character.java
+++ server/src/main/java/com/fairytale/FairyTale/domain/character/domain/Character.java
@@ -2,22 +2,33 @@ package com.fairytale.FairyTale.domain.character.domain;
 
 import com.fairytale.FairyTale.domain.book.domain.Book;
 import com.fairytale.FairyTale.domain.letter.domain.Letter;
-import com.fairytale.FairyTale.domain.storyinteraction.domain.StoryInteraction;
 import com.fairytale.FairyTale.global.database.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Table(name = "Characters")
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Character extends BaseEntity {
+
+    @OneToMany(mappedBy = "character", cascade = CascadeType.ALL)
+    private final List<Letter> letterList = new ArrayList<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,8 +48,8 @@ public class Character extends BaseEntity {
     @Column(name = "ai_prompt")
     private String aiPrompt;
 
-    @OneToMany(mappedBy = "character", cascade = CascadeType.ALL)
-    private final List<Letter> letterList = new ArrayList<>();
+    @Column(name = "voice_type_id")
+    private Integer voiceTypeId;
 
     @Builder
     public Character(Book book, String name, String imageUrl, String aiPrompt) {
