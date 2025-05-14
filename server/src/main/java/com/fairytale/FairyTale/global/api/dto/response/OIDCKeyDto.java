2f8aa9b7285b00bd2dbb5ff0377b64f0f6b49c98
Baek Gi Chan <laphayen@gmail.com>
2025-05-14 09:42:01 +0900

    fix: OIDCKeyDto에 @JsonIgnoreProperties 추가하여 불필요한 필드 무시
    Kakao OIDC 응답에서 불필요하거나 예상치 못한 필드로 인한 역직렬화 오류 방지
    `@JsonIgnoreProperties(ignoreUnknown = true)` 적용

--- server/src/main/java/com/fairytale/FairyTale/global/api/dto/response/OIDCKeyDto.java
+++ server/src/main/java/com/fairytale/FairyTale/global/api/dto/response/OIDCKeyDto.java
@@ -1,11 +1,13 @@
 package com.fairytale.FairyTale.global.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OIDCKeyDto {
 
     @JsonProperty("kid")
