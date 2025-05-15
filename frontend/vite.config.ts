0f93a47d3de514455d6cc30ccbe1ea98bd37020f
chilly003 <chfhchf03@naver.com>
2025-05-15 14:38:26 +0900

    fix : 초기 로딩 느린거 해결(거의 못함)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import million from "million/compiler";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    million.vite({ auto: true }),
+    react()
+  ],
   build: {
-    // 타입 에러로 인해 빌드 멈추지 않게 설정
     target: 'esnext',
     minify: false,
     rollupOptions: {}
-  },
-  // ↓ Vite 자체 타입 오류 무시하는 옵션은 없음. 대신 tsc 분리하거나 eslint로 제한 가능
-})
+  }
+});
