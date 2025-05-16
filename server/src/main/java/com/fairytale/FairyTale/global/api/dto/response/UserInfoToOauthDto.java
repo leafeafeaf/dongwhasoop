3a500d1cc6505498d0ec221184e93079f7036370
Baek Gi Chan <laphayen@gmail.com>
2025-05-16 17:53:29 +0900

    feat: 회원탈퇴 역직렬화 오류 수정

--- server/src/main/java/com/fairytale/FairyTale/global/api/dto/response/UserInfoToOauthDto.java
+++ server/src/main/java/com/fairytale/FairyTale/global/api/dto/response/UserInfoToOauthDto.java
@@ -1,11 +1,14 @@
 package com.fairytale.FairyTale.global.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserInfoToOauthDto {
 
     private String id;
+
 }
