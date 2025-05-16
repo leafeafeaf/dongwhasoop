2c710a36d412b4a7ea5a1abecfa9a9793860aa35
오승열 <tmdduf785@naver.com>
2025-05-16 17:18:19 +0900

    fix: loginbackground preload [#S12P31B202-frontend]

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import BookText from "../assets/images/loginpage/booktext.webp";
 import KaKaoLogin from "../components/commons/KaKaoLoginButton.tsx";
+import LoginBackGround from "../assets/images/loginpage/loginbackground.webp";
 
 function Login() {
   return (
     <div
       className="fixed inset-0 w-screen h-screen bg-cover bg-center text-white"
-      style={{ backgroundImage: "url('/backgrounds/loginbackground.webp')" }}
+      style={{ backgroundImage: `url(${LoginBackGround})` }}
     >
       {/* 동화숲 로고 */}
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
