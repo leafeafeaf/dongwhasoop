96698111c6c85fdfd27c2c9686a5e88da45f5d6a
CHJ <shine8648@naver.com>
2025-05-19 10:56:02 +0900

    fix: 볼륨 주석 처리

--- frontend/src/stores/musicStore.ts
+++ frontend/src/stores/musicStore.ts
@@ -3,11 +3,15 @@ import btnSound from '../assets/music/btn_sound.mp3';
 
 interface MusicState {
   isPlaying: boolean;
+  volume: number;
+  setVolume: (volume: number) => void;
   togglePlay: () => void;
 }
 
 export const useMusicStore = create<MusicState>((set) => ({
   isPlaying: false,
+  volume: 1,
+  setVolume: (volume) => set({ volume }),
   togglePlay: () => {
     const sound = new Audio(btnSound);
     sound.play();
